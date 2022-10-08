69369fbf6c3089997c80ab10235ebfda34eaf481
Ayush Chandra <71286983+waknuckles1020@users.noreply.github.com>
2022-10-08 17:16:01 -0500

    fix intake

--- compCode1/src/subsystemCodes/intake.cpp
+++ compCode1/src/subsystemCodes/intake.cpp
@@ -6,10 +6,9 @@ void setIntakeMotors(){
     //forward
     if(Controller1.get_digital(pros::E_CONTROLLER_DIGITAL_L2)){
         intake = 100;
-    }
-
-    //backwaard
-    if(Controller1.get_digital(pros::E_CONTROLLER_DIGITAL_L1)){
+    } else if(Controller1.get_digital(pros::E_CONTROLLER_DIGITAL_L1)){
         intake = -100;
+    } else {
+        intake = 0;
     }
 }
