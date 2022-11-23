dd8ebe82c9eee2f35561b3b05cbca28e405050f0
waknuckles1020 <71286983+waknuckles1020@users.noreply.github.com>
2022-11-22 21:26:23 -0600

    new comp

---
+++ compTempRound2/include/robot-config.h
@@ -0,0 +1,25 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern controller Controller1;
+extern motor RFM;
+extern motor RBM;
+extern motor LFM;
+extern motor LBM;
+extern encoder rightTrack;
+extern encoder centTrack;
+extern encoder leftTrack;
+extern motor intake;
+extern motor indexer;
+extern motor fly1;
+extern motor endgame;
+extern inertial inert1;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
